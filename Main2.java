d6933d5aec13f02cea30bf1934aa483884bcf3d8
Gabemgl20 <gab24mig08ri10@gmail.com>
2026-05-09 11:21:02 -0300

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner cz = new Scanner(System.in);
+		
+		int id1;
+		int id2;
+		
+		System.out.println("Qual é a primeira idade?");
+		id1 = cz.nextInt();
+		System.out.println("Qual é a segunda idade?");
+		id2 = cz.nextInt();
+		if (id1 == id2)
+		{
+		    System.out.println("As duas são iguais");
+		}
+		else
+		{
+		    System.out.println("As duas são diferentes");
+		}
+		if (id1 >= 18)
+		{
+		    System.out.println("A primeira é maior de idade");
+		}
+		else
+		{
+		    System.out.println("A primeira é menor de idade");
+		}
+		if (id2 <= 12)
+		{
+		    System.out.println("A segunda é menor de 12");
+		}
+		else
+		{
+		    System.out.println("A segunda é maior de 12");
+		}
+		
+		
+	}
+}
