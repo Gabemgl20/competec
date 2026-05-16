84a791bbaaf0d0b6c07cc9768b48fbfc755aaba3
Gabemgl20 <gab24mig08ri10@gmail.com>
2026-05-16 10:24:02 -0300

    Add files via upload

---
+++ Main3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner cz = new Scanner (System.in);
+		
+		int x=1;
+		
+		while (x<=5)
+		{
+		    System.out.println("Oi meu nome é Gabriel");
+		    
+		    x++;
+		}
+		
+	}
+}
